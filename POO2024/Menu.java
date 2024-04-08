b795473eebe5365740fa4de52358a63a3dca6fdc
Miguel Vila <79595104+miguelvila02@users.noreply.github.com>
2024-04-08 16:24:43 +0100

    brrrrrrrrrrr
    Main primeiro menu e retroceder feitos depois trato de fazer os outros

--- POO2024/Menu.java
+++ POO2024/Menu.java
@@ -31,8 +31,11 @@ public class Menu
         switch(getOpcao()){
             case 0:
                 System.out.println("Adeus!");
+                break;
             case 1:
+                System.out.println("passei aqui");
                 this.menuCriacao();
+                break;
         }
     }
 
@@ -44,9 +47,55 @@ public class Menu
 
         switch(valor){
             case 0:
-                menuInicial();
+                this.menuInicial();
+                break;
+            case 1:
+                this.menuuserCreation();
+                break;
+            case 2:
+                this.menuactivityCreation();
+                break;
+            case 3:
+                this.menuplanningCreation();
+                break;
+        }
+    }
+    public void menuuserCreation()
+    {
+        userCreation();
+        lerOpcao();
+        int valor = getOpcao();
 
+        switch(valor){
+            case 0:
+                this.menuCriacao();
+                break;
+        }
+    }
 
+    public void menuactivityCreation()
+    {
+        activityCreation();
+        lerOpcao();
+        int valor = getOpcao();
+
+        switch(valor){
+            case 0:
+                this.menuCriacao();
+                break;
+        }
+    }
+
+    public void menuplanningCreation()
+    {
+        planningCreation();
+        lerOpcao();
+        int valor = getOpcao();
+
+        switch(valor){
+            case 0:
+                this.menuCriacao();
+                break;
         }
     }
 
@@ -66,12 +115,44 @@ public class Menu
     private void showCriacao()
     {
         System.out.println("\n *** Criacao ***");
-        System.out.println("1 - Utilizador:");
-        System.out.println("2 - Atividades:");
-        System.out.println("3 - Planos de treino:");
+        System.out.println("1 - Utilizador");
+        System.out.println("2 - Atividades");
+        System.out.println("3 - Planos de treino");
         System.out.println("0 - Voltar");
     }
     /** Ler uma opcao valida */
+
+    private void userCreation()
+    {
+        System.out.println("\n *** Criacao Utilizador***");
+        System.out.println("Insere o nome:");
+        System.out.println("Sexo:");
+        System.out.println("Morada:");
+        System.out.println("Email");
+        System.out.println("Frequencia Cardiaca Media");
+        System.out.println("0 - Voltar");
+
+    }
+
+    private void activityCreation()
+    {
+        System.out.println("\n *** Criacao Atividade***");
+        System.out.println("Tipo da atividade:");
+        System.out.println("Nome da atividade:");
+        System.out.println("Dificuldade:");
+        System.out.println("0 - Voltar");
+    }
+
+    private void planningCreation()
+    {
+        System.out.println("\n *** Criacao Planos de Atividades***");
+        System.out.println("Dia(s) da semana:");
+        System.out.println("Caso não queira inserir plano ou  exercicios, na respetiva ordem escreva 1");
+        System.out.println("Plano a seguir:");
+        System.out.println("Caso não queira seguir um plano em concreto ou adicionar exercícios, especifique abaixo");
+        System.out.println("Exercicio(s):");
+        System.out.println("0 - Voltar");
+    }
     private void lerOpcao()
     {
         int op;
